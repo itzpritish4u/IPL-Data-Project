0c2c56ca55e9fb77dff5f42f58114fc5f17dced2
itzpritish4u <rajpritish75@gmail.com>
2024-09-16 23:56:29 +0530

    Formed the solution and generated the output for 1-matches-per-year.js

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -5,28 +5,27 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = join(__filename, '..');
 
-const matchesPath = join(__dirname, '../data/matches.csv');
+const matchesJSONPath = join(__dirname, '../data/matches.json');
 const outputPath = join(__dirname, '../public/output/matchesPerYear.json');
 
-function countMatchesPerYear(matches) {
+function getMatchesPerYear(matches) {
   const matchesPerYear = {};
-  for (let i = 1; i < matches.length; i++) {
-    const matchDetails = matches[i].split(',');
-    const year = matchDetails[1];
-    if (year && year !== '') {
-      if (matchesPerYear[year]) {
-        matchesPerYear[year]++;
-      } else {
-        matchesPerYear[year] = 1;
-      }
+
+  matches.forEach(match => {
+    const season = match.season;
+    if (matchesPerYear[season]) {
+      matchesPerYear[season]++;
+    } else {
+      matchesPerYear[season] = 1;
     }
-  }
+  });
+
   return matchesPerYear;
 }
 
 function main() {
-  const matches = readFileSync(matchesPath, 'utf-8').split('\n');
-  const matchesPerYear = countMatchesPerYear(matches);
+  const matches = JSON.parse(readFileSync(matchesJSONPath, 'utf-8'));
+  const matchesPerYear = getMatchesPerYear(matches);
   writeFileSync(outputPath, JSON.stringify(matchesPerYear, null, 2), 'utf-8');
   console.log('matchesPerYear.json has been created successfully.');
 }
