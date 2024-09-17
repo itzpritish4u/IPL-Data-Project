61af33453632d33b142a04e1591c2ff049169359
itzpritish4u <rajpritish75@gmail.com>
2024-09-17 16:29:57 +0530

    Improved the implementation for matches-per-year.js

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -1,30 +1,23 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
+import { writeFileSync } from 'fs';
+import matches from '../data/matches.json' assert { type: 'json' };
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = join(__filename, '..');
-
-const matchesJSONPath = join(__dirname, '../data/matches.json');
-const outputPath = join(__dirname, '../public/output/matchesPerYear.json');
+const outputPath = './src/public/output/matchesPerYear.json';
 
 function getMatchesPerYear(matches) {
   const matchesPerYear = {};
 
   matches.forEach(match => {
     const season = match.season;
-    if (matchesPerYear[season]) {
-      matchesPerYear[season]++;
-    } else {
-      matchesPerYear[season] = 1;
+    if (!matchesPerYear[season]) {
+      matchesPerYear[season] = 0;
     }
+    matchesPerYear[season]++;
   });
 
   return matchesPerYear;
 }
 
 function main() {
-  const matches = JSON.parse(readFileSync(matchesJSONPath, 'utf-8'));
   const matchesPerYear = getMatchesPerYear(matches);
   writeFileSync(outputPath, JSON.stringify(matchesPerYear, null, 2), 'utf-8');
   console.log('matchesPerYear.json has been created successfully.');
