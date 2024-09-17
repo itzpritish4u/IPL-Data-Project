765fe5590e31024ff84b0a85896cd3a01cb01f83
itzpritish4u <rajpritish75@gmail.com>
2024-09-17 10:38:55 +0530

    Formed the solution and generated the output for extraRunsConceded2016.js

--- src/server/3-extra-runs-conceded-per-team-in-the-year-2016.js
+++ src/server/3-extra-runs-conceded-per-team-in-the-year-2016.js
@@ -5,43 +5,40 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = join(__filename, '..');
 
-const matchesPath = join(__dirname, '../data/matches.csv');
-const deliveriesPath = join(__dirname, '../data/deliveries.csv');
+const matchesPath = join(__dirname, '../data/matches.json');
+const deliveriesPath = join(__dirname, '../data/deliveries.json');
 const outputPath = join(__dirname, '../public/output/extraRunsConceded2016.json');
 
-function get2016MatchesIDs(matches) {
+function get2016MatchesIDs(matches, year) {
     const matchIDs = [];
-    for (let i = 1; i < matches.length; ++i) {
-        const match = matches[i].split(',');
-        if (match[1] === '2016') {   // match[1] - year
-            matchIDs.push(match[0]); // match[0] - ID
+    matches.forEach(match => {
+        if (match.season === year) {
+            matchIDs.push(match.id);
         }
-    }
+    });
     return matchIDs;
 }
 
 function getExtraRunsConceded(deliveries, matchIDs) {
     const extraRunsConceded = {};
-    for (let i = 1; i < deliveries.length; ++i) {
-        const delivery = deliveries[i].split(',');
-        
-        if (matchIDs.includes(delivery[0])) { // delivery[0] is matchID
-            const ballingTeam = delivery[3];
-            if (extraRunsConceded[ballingTeam]) {
-                extraRunsConceded[ballingTeam] += parseInt(delivery[16], 10);
-            } else {
-                extraRunsConceded[ballingTeam] = parseInt(delivery[16], 10);
+    deliveries.forEach(delivery => {
+        if (matchIDs.includes(delivery.match_id)) {
+            const ballingTeam = delivery.bowling_team;
+
+            if (!extraRunsConceded[ballingTeam]) {
+                extraRunsConceded[ballingTeam] = 0;
             }
+            extraRunsConceded[ballingTeam] += parseInt(delivery.extra_runs, 10);
         }
-    }
+    });
 
     return extraRunsConceded;
 }
 
 function main() {
-    const matches = readFileSync(matchesPath, 'utf-8').split('\n');
-    const deliveries = readFileSync(deliveriesPath, 'utf-8').split('\n');
-    const matchIDs = get2016MatchesIDs(matches);
+    const matches = JSON.parse(readFileSync(matchesPath, 'utf-8'));
+    const deliveries = JSON.parse(readFileSync(deliveriesPath, 'utf-8'));
+    const matchIDs = get2016MatchesIDs(matches, '2016');
     const extraRunsConceded = getExtraRunsConceded(deliveries, matchIDs);
     writeFileSync(outputPath, JSON.stringify(extraRunsConceded, null, 4), 'utf-8');
     console.log('extraRunsConceded2016.json has been created successfully.');
