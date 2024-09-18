2829f43d5ef376228ff37dd0fb8b59d8dbcb539a
itzpritish4u <rajpritish75@gmail.com>
2024-09-18 12:54:59 +0530

    Implemented Higher-Order function of playerWonHighestPOM solution

--- src/server/6-player-won-highest-number-of-player-of-match.js
+++ src/server/6-player-won-highest-number-of-player-of-match.js
@@ -1,50 +1,45 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
+import { writeFileSync } from 'fs';
+import matches from '../data/matches.json' assert { type: 'json' };
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = join(__filename, '..');
-
-const matchesPath = join(__dirname, '../data/matches.csv');
-const outputPath = join(__dirname, '../public/output/PlayerWonHighestPOM.json');
+const outputPath = './src/public/output/playerWonHighestPOM.json';
 
 function getPlayerWonHighestPOM(matches) {
-    const playerWhoWonPOM = {};
-    for (let i = 1; i < matches.length; ++i) {
-        const match = matches[i].split(',');
-        const season = match[1];
-        const POM = match[13];
+    const playerWhoWonPOM = matches.reduce((acc, match) => {
+        const season = match.season;
+        const POM = match.player_of_match;
 
-        if (season && !playerWhoWonPOM[season]) {
-            playerWhoWonPOM[season] = {};
+        if (season && !acc[season]) {
+            acc[season] = {};
         }
 
         if (POM) {
-            if (playerWhoWonPOM[season][POM]) {
-                playerWhoWonPOM[season][POM]++;
-            } else {
-                playerWhoWonPOM[season][POM] = 1;
+            if (!acc[season][POM]) {
+                acc[season][POM] = 0;
             }
+            acc[season][POM]++;
         }
-    }
+        return acc;
+    }, {});
 
-    const playerWonHighestPOM = {};
-    for (let season in playerWhoWonPOM) {
+    const mostPOM = Object.entries(playerWhoWonPOM).reduce((acc, [season, players]) => {
         let maxAwards = 0;
         let PlayerOfMatch = '';
-        for (let player in playerWhoWonPOM[season]) {
-            if (playerWhoWonPOM[season][player] > maxAwards) {
-                maxAwards = playerWhoWonPOM[season][player];
+
+        Object.entries(players).forEach(([player, awards]) => {
+            if (awards > maxAwards) {
+                maxAwards = awards;
                 PlayerOfMatch = player;
             }
-        }
-        playerWonHighestPOM[season] = {PlayerOfMatch :  PlayerOfMatch, awards : maxAwards};
-    }
-    return playerWonHighestPOM;
+        });
+
+        acc[season] = { PlayerOfMatch, awards: maxAwards };
+        return acc;
+    }, {});
+
+    return mostPOM;
 }
 
 function main() {
-    const matches = readFileSync(matchesPath, 'utf-8').split('\n');
     const playerWonHighestPOM = getPlayerWonHighestPOM(matches);
     writeFileSync(outputPath, JSON.stringify(playerWonHighestPOM, null, 2), 'utf-8');
     console.log('PlayerWonHighestPOM.json has been created successfully.');
