68a4ed5c4ec5ae5641a5428c9e358b3a69ef2b84
itzpritish4u <rajpritish75@gmail.com>
2024-09-18 10:18:19 +0530

    Implemented Higher-Order function extraRunsConceded2016 solution

--- src/server/3-extra-runs-conceded-per-team-in-the-year-2016.js
+++ src/server/3-extra-runs-conceded-per-team-in-the-year-2016.js
@@ -1,27 +1,17 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
+import { writeFileSync } from 'fs';
+import matches from '../data/matches.json' assert { type: 'json' };
+import deliveries from '../data/deliveries.json' assert { type: 'json' };
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = join(__filename, '..');
-
-const matchesPath = join(__dirname, '../data/matches.json');
-const deliveriesPath = join(__dirname, '../data/deliveries.json');
-const outputPath = join(__dirname, '../public/output/extraRunsConceded2016.json');
+const outputPath = './src/public/output/extraRunsConceded2016.json';
 
 function get2016MatchesIDs(matches, year) {
-    const matchIDs = [];
-    matches.forEach(match => {
-        if (match.season === year) {
-            matchIDs.push(match.id);
-        }
-    });
-    return matchIDs;
+    return matches
+        .filter(match => match.season === year)
+        .map(match => match.id);
 }
 
 function getExtraRunsConceded(deliveries, matchIDs) {
-    const extraRunsConceded = {};
-    deliveries.forEach(delivery => {
+    return deliveries.reduce((extraRunsConceded, delivery) => {
         if (matchIDs.includes(delivery.match_id)) {
             const ballingTeam = delivery.bowling_team;
 
@@ -30,14 +20,11 @@ function getExtraRunsConceded(deliveries, matchIDs) {
             }
             extraRunsConceded[ballingTeam] += parseInt(delivery.extra_runs, 10);
         }
-    });
-
-    return extraRunsConceded;
+        return extraRunsConceded;
+    }, {});
 }
 
 function main() {
-    const matches = JSON.parse(readFileSync(matchesPath, 'utf-8'));
-    const deliveries = JSON.parse(readFileSync(deliveriesPath, 'utf-8'));
     const matchIDs = get2016MatchesIDs(matches, '2016');
     const extraRunsConceded = getExtraRunsConceded(deliveries, matchIDs);
     writeFileSync(outputPath, JSON.stringify(extraRunsConceded, null, 4), 'utf-8');
