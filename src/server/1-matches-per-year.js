c40d4cdf138024c3253bdcae834f0ca3b6dc1915
itzpritish4u <rajpritish75@gmail.com>
2024-09-18 10:02:31 +0530

    Implemented Higher-Order Functions

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -4,17 +4,14 @@ import matches from '../data/matches.json' assert { type: 'json' };
 const outputPath = './src/public/output/matchesPerYear.json';
 
 function getMatchesPerYear(matches) {
-  const matchesPerYear = {};
-
-  matches.forEach(match => {
+  return matches.reduce((matchesPerYear, match) => {
     const season = match.season;
     if (!matchesPerYear[season]) {
       matchesPerYear[season] = 0;
     }
     matchesPerYear[season]++;
-  });
-
-  return matchesPerYear;
+    return matchesPerYear;
+  }, {});
 }
 
 function main() {
--- src/server/2-matches-won-per-team-per-year.js
+++ src/server/2-matches-won-per-team-per-year.js
@@ -1,16 +1,10 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
+import { writeFileSync } from 'fs';
+import matches from '../data/matches.json' assert { type: 'json' };
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = join(__filename, '..');
-
-const matchesPath = join(__dirname, '../data/matches.json');
-const outputPath = join(__dirname, '../public/output/matchesWonPerTeamPerYear.json');
+const outputPath = './src/public/output/matchesWonPerTeamPerYear.json';
 
 function getMatchesWonPerTeamPerYear(matches) {
-    const matchesWonPerTeamPerYear = {};
-    matches.forEach(match => {
+    return matches.reduce((matchesWonPerTeamPerYear, match) => {
         const season = match.season;
         const winner = match.winner;
 
@@ -23,12 +17,11 @@ function getMatchesWonPerTeamPerYear(matches) {
             }
             matchesWonPerTeamPerYear[season][winner]++;
         }
-    });
-    return matchesWonPerTeamPerYear;
+        return matchesWonPerTeamPerYear;
+    }, {});
 }
 
 function main() {
-    const matches = JSON.parse(readFileSync(matchesPath, 'utf-8'));
     const matchesWonPerTeamPerYear = getMatchesWonPerTeamPerYear(matches);
     writeFileSync(outputPath, JSON.stringify(matchesWonPerTeamPerYear, null, 2), 'utf-8');
     console.log('matchesWonPerTeamPerYear.json has been created successfully.');
