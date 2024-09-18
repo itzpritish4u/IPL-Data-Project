e72f5df4c487eaf8a663e062ea8f838dd3bee8de
itzpritish4u <rajpritish75@gmail.com>
2024-09-18 11:48:05 +0530

    Implemented Higher-Order function of teamsWonTossAndMatch solution

--- src/server/5-teams-won-toss-and-match.js
+++ src/server/5-teams-won-toss-and-match.js
@@ -1,33 +1,24 @@
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
+import { writeFileSync } from 'fs';
+import matches from '../data/matches.json' assert { type: 'json' };
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = join(__filename, '..');
-
-const matchesPath = join(__dirname, '../data/matches.csv');
-const outputPath = join(__dirname, '../public/output/TeamsWonTossAndMatch.json');
+const outputPath = './src/public/output/teamsWonTossAndMatch.json';
 
 function getTeamsWonTossAndMatch(matches) {
-    const tossAndMatchWins = {};
-    for (let i = 1; i < matches.length; ++i) {
-        const match = matches[i].split(',');
-        const tossWinner = match[6];
-        const matchWinner = match[10];
+    return matches.reduce((tossAndMatchWins, match) => {
+        const tossWinner = match.toss_winner;
+        const matchWinner = match.winner;
 
         if (tossWinner && matchWinner && tossWinner === matchWinner) {
             if (!tossAndMatchWins[tossWinner]) {
-                tossAndMatchWins[tossWinner] = 1;
-            } else {
-                tossAndMatchWins[tossWinner]++;
+                tossAndMatchWins[tossWinner] = 0;
             }
+            tossAndMatchWins[tossWinner]++;
         }
-    }
-    return tossAndMatchWins;
+        return tossAndMatchWins;
+    }, {});
 }
 
 function main() {
-    const matches = readFileSync(matchesPath, 'utf-8').split('\n');
     const TeamsWonTossAndMatch = getTeamsWonTossAndMatch(matches);
     writeFileSync(outputPath, JSON.stringify(TeamsWonTossAndMatch, null, 2), 'utf-8');
     console.log('TeamsWonTossAndMatch.json has been created successfully.');
